4d71df222f3af3730abc5faa47e01e180ac27f68
alqusairy <tugasrpl19@gmail.com>
2022-04-21 10:04:38 +0700

    Initial commit

---
+++ app/src/main/java/zizi/polbeng/ac/id/eventhandlingexample/MainActivity.kt
@@ -0,0 +1,23 @@
+package zizi.polbeng.ac.id.eventhandlingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDisplayMessage.setOnClickListener {
+            Toast.makeText(
+                applicationContext, "Hello World",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        btnDisplayMessage.setOnLongClickListener { true }
+    }
+}
